813b5857f955707fec9a1154c5a348f9d17bdc91
justinmorales1 <jm.em0210@gmail.com>
2022-08-22 21:28:30 -0500

    Adding function to handle movie input

--- src/components/MovieSearchInput.jsx
+++ src/components/MovieSearchInput.jsx
@@ -2,22 +2,31 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import {Field, reduxForm} from "redux-form";
 
+
+const renderTextField = (
+    { input, label, meta: { touched, error }, ...custom },
+) => (
+    <TextField
+        hintText={label}
+        floatingLabelText={label}
+        errorText={touched && error}
+        {...input}
+        {...custom}
+    />
+);
+
 const MovieSearchInput = (props) => {
     const { handleSubmit} = props;
 
     return (
         <form onSubmit={handleSubmit}>
             <div>
-                <TextField type="text" id="outlined-basic" label="Outlined" variant="outlined" />
-
-                {/*<TextField name="movieInput" type="text" id="outlined-basic" label="Outlined" variant="outlined" />*/}
-            </div>
-            <div>
+                {/*<TextField type="text" id="outlined-basic" label="Outlined" variant="outlined" />*/}
                 <Field
-                    name="firstName"
-                    component="input"
+                    name="movieInput"
+                    component={renderTextField}
+                    label="Movie Input"
                     type="text"
-                    placeholder="First Name"
                 />
             </div>
         </form>
