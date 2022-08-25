1bea167039f2921837d1c914dc7f2bac031bd0b1
justinmorales1 <jm.em0210@gmail.com>
2022-08-25 18:48:34 -0500

    Adding some movie initial state

--- src/redux/reducers/watchListMovieReducer.js
+++ src/redux/reducers/watchListMovieReducer.js
@@ -1,7 +1,13 @@
 import {REMOVE_MOVIE_FROM_WATCH_LIST, WATCH_LIST_MOVIE} from "../../constants/constans";
 
+const initialState = [
+    { id: 1, Title: 'Starship Troop', Year: '1990', Plot: "Its about a war with Arachnids" },
+    { id: 2, Title: 'Game of Thrones', Year: '2009', Plot: "Its a game for a throne" },
+    { id: 3, Title: 'Jaws', Year: '2017', Plot: "Its a movie about a killer shark." },
+    { id: 4, Title: 'South Park', Year: '1994', Plot: "Its a show about 4 kids from south park." },
+]
 
-const watchListMovieReducer = (state = [], action) => {
+const watchListMovieReducer = (state = initialState, action) => {
     switch (action.type) {
         case WATCH_LIST_MOVIE:
             return [...state, action.payload];
